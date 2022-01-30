b243df96da4def10232f12fc8bce333966c24213
Jburg1414 <Jburgess1414@gmail.com>
2022-01-29 22:08:38 -0700

    got text input to save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,11 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 var currentTime = moment().hour();
 console.log(currentTime);
 
-// use set interval for updating color
-
 function colorUpdate() {
     $(".time-block").each(function(){ 
         if ($(this).children(".hour").data("time") < currentTime) {
             $(this).children(".description").addClass("past");
-        } else if ($(this).children(".hour").data("time") = currentTime) {
+        } else if ($(this).children(".hour").data("time") == currentTime) {
             $(this).children(".description").addClass("present");
         } else {
             $(this).children(".description").addClass("future");
@@ -17,7 +15,17 @@ function colorUpdate() {
     });
 };
 
-
 // one event listener for saveBtn
 
+$(".saveBtn").click(function(){
+    var saveText = $(this).siblings(".description").val();
+    console.log(saveText);
+    var selectedTime = $(this).siblings(".hour").data("time");
+    localStorage.setItem(selectedTime, saveText);
+    
+});
+
+// return localStorage.getItem(selectedTime, saveText);
+
+// $("container").
 colorUpdate();
